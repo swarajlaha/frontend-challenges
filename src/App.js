5cbb4c641582dea73c57b629c2226b0a506d21aa
swarajlaha <swaraj.srj@gmail.com>
2021-07-10 22:14:48 +0530

    import, render main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import AppNavbar from './commons/appNavbar';
+import Main from './pages/Main';
 
 function App() {
   return (
     <>
       <AppNavbar />
+      <Main />
     </>
   );
 }
