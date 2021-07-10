efb093442f54bddb645aab9a8dddb4d721e807bd
swarajlaha <swaraj.srj@gmail.com>
2021-07-10 22:15:08 +0530

    render displaybox page

--- src/commons/appNavbar.js
+++ src/commons/appNavbar.js
@@ -4,7 +4,7 @@ import { Navbar } from 'react-bootstrap';
 const AppNavbar = () => {
   return (
     <>
-      <Navbar bg="dark" variant="dark" className='mt-2 ml-2 mr-2 mb-3'>
+      <Navbar bg="dark" variant="dark" className='mt-2 ml-2 mr-2'>
         <Navbar.Brand href="#home">
           Password Checker
         </Navbar.Brand>
---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DisplayBox from '../containers/displayBox';
+
+const Main = () => {
+  return (
+    <>
+      <DisplayBox />
+    </>
+  );
+}
+
+export default Main;
