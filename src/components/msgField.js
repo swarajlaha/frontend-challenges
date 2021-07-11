61c2a4f66137ee42cae2e837b81f9aacd355fad7
swarajlaha <swaraj.srj@gmail.com>
2021-07-11 14:42:12 +0530

    conditionally render pswd criteria

--- src/components/msgField.js
+++ src/components/msgField.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import { Alert } from 'react-bootstrap'
 
-const MsgField = () => {
+const MsgField = ({ pswdCheck, pswdLenCheck }) => {
   return (
     <>
       <Alert variant="success">
         <Alert.Heading>Password Criteria</Alert.Heading>
-        <p>
-          - Min 8 characters <br />
-          - Should contain atleast 1 number <br />
-          - Should contain atleast 1 speacial character
-        </p>
+        <ul>
+        {pswdLenCheck ? (<li className='mb-0'>Min 8 characters </li>) : (<li className='mb-0' style={{ color: 'red' }}>Min 8 characters </li>)}
+        {pswdCheck.numCheck ? (<li className='mb-0'>Should contain atleast 1 number</li>) : (<li className='mb-0' style={{ color: 'red' }}>Should contain atleast 1 number</li>)}
+        {pswdCheck.charCheck ? (<li className='mb-0'>Should contain atleast 1 alphabet </li>) : (<li className='mb-0' style={{ color: 'red' }}>Should contain atleast 1 alphabet </li>)}
+        {pswdCheck.specialCharCheck ? (<li className='mb-0'>Should contain atleast 1 special character</li>) : (<li className='mb-0' style={{ color: 'red' }}>Should contain atleast 1 special character</li>)}
+        </ul>
         <hr />
-        <Alert.Heading>Password Check</Alert.Heading>
-        <p>
-          To show password check result...
-        </p>
+        <Alert.Heading>Password Strength</Alert.Heading>
+        <p>To show password strength...</p>
       </Alert>
     </>
   )
