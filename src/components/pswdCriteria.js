c3abf71b4cf3b641a65be23c5561b5913c4d1ad6
swarajlaha <swaraj.srj@gmail.com>
2021-07-11 15:04:55 +0530

    add cross, tick icons

--- src/components/pswdCriteria.js
+++ src/components/pswdCriteria.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { TiTick } from 'react-icons/ti' 
+import { ImCross } from 'react-icons/im'
 
 const PswdCriteria = ({ pswdCheck, pswdLenCheck }) => {
   return (
     <>
       <ul>
-        {pswdLenCheck ? (<li className='mb-0'>Min 8 characters </li>) : (<li className='mb-0' style={{ color: 'red' }}>Min 8 characters </li>)}
-        {pswdCheck.numCheck ? (<li className='mb-0'>Should contain atleast 1 number</li>) : (<li className='mb-0' style={{ color: 'red' }}>Should contain atleast 1 number</li>)}
-        {pswdCheck.charCheck ? (<li className='mb-0'>Should contain atleast 1 alphabet </li>) : (<li className='mb-0' style={{ color: 'red' }}>Should contain atleast 1 alphabet </li>)}
-        {pswdCheck.specialCharCheck ? (<li className='mb-0'>Should contain atleast 1 special character</li>) : (<li className='mb-0' style={{ color: 'red' }}>Should contain atleast 1 special character</li>)}
+        {pswdLenCheck ? (<li className='mb-0'>Min 8 characters <TiTick size={22} /></li>) : (<li className='mb-0' style={{ color: 'red' }}>Min 8 characters <ImCross size={12} /></li>)}
+        {pswdCheck.numCheck ? (<li className='mb-0'>Should contain atleast 1 number <TiTick size={22} /></li>) : (<li className='mb-0' style={{ color: 'red' }}>Should contain atleast 1 number <ImCross size={12} /></li>)}
+        {pswdCheck.charCheck ? (<li className='mb-0'>Should contain atleast 1 alphabet <TiTick size={22} /></li>) : (<li className='mb-0' style={{ color: 'red' }}>Should contain atleast 1 alphabet <ImCross size={12} /></li>)}
+        {pswdCheck.specialCharCheck ? (<li className='mb-0'>Should contain atleast 1 special character <TiTick size={22} /></li>) : (<li className='mb-0' style={{ color: 'red' }}>Should contain atleast 1 special character <ImCross size={12} /></li>)}
         </ul>
     </>
   )
