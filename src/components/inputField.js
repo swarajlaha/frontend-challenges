4fe399744d7d7e7fd13c567b9245890b2cf8aed2
swarajlaha <swaraj.srj@gmail.com>
2021-07-24 20:15:31 +0530

    show stars when hide pswd is true

--- src/components/inputField.js
+++ src/components/inputField.js
@@ -1,7 +1,22 @@
-import React from 'react';
-import { InputGroup, FormControl, Button } from 'react-bootstrap';
+import React, { useState } from 'react'
+import { InputGroup, FormControl, Button } from 'react-bootstrap'
+import { BiHide, BiShow } from 'react-icons/bi'
+import { BsDot } from 'react-icons/bs'
 
 const InputField = ({ getLastChar, pswdCheck, pswdLenCheck }) => {
+  const [isPswdhidden, setIsPswdhidden] = useState(false)
+  const [enteredTxt, setEnteredTxt] = useState('')
+  const [enteredStar, setEnteredStar] = useState('')
+
+  const inputchangeHandler = (e) => {
+    getLastChar(e.target.value)
+    setEnteredTxt(e.target.value)
+    let noOfStars = '*'
+    for(let i = 0; i < enteredTxt.length; i ++) {
+      noOfStars += '*'
+    }
+    setEnteredStar(noOfStars)
+  }
 
   return (
     <>
@@ -10,14 +25,33 @@ const InputField = ({ getLastChar, pswdCheck, pswdLenCheck }) => {
           placeholder="Type Password..."
           aria-label="Type Password"
           aria-describedby="basic-addon2"
-          onChange={(e) => getLastChar(e.target.value)}
+          onChange={(e) => inputchangeHandler(e)}
+          value={isPswdhidden ? enteredStar : enteredTxt}
         />
+        <Button
+          variant="outline-dark"
+          onClick={() => setIsPswdhidden(!isPswdhidden)}
+        >
+          {isPswdhidden ? <BiShow /> : <BiHide />}
+        </Button>
         <InputGroup.Append>
-          <Button variant="outline-success" disabled={!(pswdCheck.charCheck && pswdCheck.numCheck && pswdCheck.specialCharCheck && pswdLenCheck)}>Save</Button>
+          <Button
+            variant="outline-success"
+            disabled={
+              !(
+                pswdCheck.charCheck &&
+                pswdCheck.numCheck &&
+                pswdCheck.specialCharCheck &&
+                pswdLenCheck
+              )
+            }
+          >
+            Save
+          </Button>
         </InputGroup.Append>
       </InputGroup>
     </>
-  );
+  )
 }
 
-export default InputField;
+export default InputField
