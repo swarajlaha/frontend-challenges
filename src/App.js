f2b88b17f58a6ba5a2a0914e36266eab5108af41
swarajlaha <swaraj.srj@gmail.com>
2021-07-25 12:12:45 +0530

    rename

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import AppNavbar from '../src/password-checker/commons/appNavbar';
 import Main from '../src/password-checker/pages/Main';
 
 function App() {
   return (
     <>
-      <AppNavbar />
       <Main />
     </>
   );
--- src/password-checker/pages/PasswordChecker.js
+++ src/password-checker/pages/PasswordChecker.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import AppNavbar from '../commons/appNavbar';
 import DisplayBox from '../containers/displayBox';
 
-const Main = () => {
+const PasswordChecker = () => {
   return (
     <>
+      <AppNavbar />
       <DisplayBox />
     </>
   );
 }
 
-export default Main;
+export default PasswordChecker;
