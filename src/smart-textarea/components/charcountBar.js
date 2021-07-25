9fc3da4f1d8072da9b82927ff68815fddc0b1cdf
swarajlaha <swaraj.srj@gmail.com>
2021-07-25 21:36:08 +0530

    show negative no. when char count exceeds limit

--- src/smart-textarea/components/charcountBar.js
+++ src/smart-textarea/components/charcountBar.js
@@ -3,8 +3,13 @@ import { ProgressBar } from 'react-bootstrap'
 
 const CharCount = ({ enteredtxt }) => {
 
-  let labeltxt = enteredtxt.length + '/50'
   let nowbar = (enteredtxt.length / 50) * 100
+  let labeltxt = ''
+  if (enteredtxt.length <= 50) {
+    labeltxt = enteredtxt.length + '/50'
+  } else {
+    labeltxt = (50 - enteredtxt.length) + '/50'
+  }
   return (
     <>
       <ProgressBar now={nowbar} label={labeltxt} />
